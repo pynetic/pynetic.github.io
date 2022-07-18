13496653a48a05b1342115aa96ae07f5e53a43e7
RhinoCodes <forsbergw82@protonmail.com>
2022-07-18 17:07:54 -0500

    Modify web page

--- main.py
+++ main.py
@@ -5,19 +5,13 @@ header = index.add(Header(
     text="Hello Internet!",
     size=1
 ))
-image = index.add(Image(
-    source="[image link]"
-))
+
 body = index.add(Paragraph(
-    text=[Link([Bold("Pynetic")], "https://github.com/Jabbey92/pynetic"), " aims to be a modern python-based framework for web development"]
+    text=[Link([Bold("Pynetic")], "https://github.com/pynetic"), " aims to be a batteries included python-based framework for web development."]
 ))
 
 button = index.add(Button(text="Testing Reactivity!"))
 button.set(
-    "onclick", reactive.button_click
-)
-
-image.set(
     "onclick", reactive.button_click
 )
 
