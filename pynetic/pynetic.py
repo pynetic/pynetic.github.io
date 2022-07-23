1d48a51ba7179034dd01636150e240efac816e3d
RhinoCodes <forsbergw82@protonmail.com>
2022-07-22 21:33:52 -0500

    Update pynetic to allow for (basic) CSR!

--- pynetic/pynetic.py
+++ pynetic/pynetic.py
@@ -1,13 +1,63 @@
-import reactive
+from uuid import uuid4 as gen_uuid
+from functools import wraps 
+from inspect import getsource
+from re import compile
+from time import time
+
+start = time()
+
+reactive = ["""import js\nfrom re import compile\n\nclass AutoRender:\n    def __init__(self, value, callback, stringToFormat, id):\n        self.xvalue = value\n        self.callback = callback\n        self.id = id\n        self.stringToFormat = stringToFormat\n    \n    @property \n    def value(self):\n        return self.xvalue\n    \n    @value.setter\n    def value(self, value):\n        js.console.log("?")\n        self.xvalue = value\n        self.callback(value, self.stringToFormat, self.id)\n\ndef update(value, stringToFormat, idx):\n    x = compile("\\{[a-z]+\\}")\n    x = x.findall(stringToFormat)\n    for i in x:\n        stringToFormat = stringToFormat.replace(i, "{}")\n    js.document.getElementById(idx).innerHTML = stringToFormat.format(value)\n\n"""]
+class AutoRender:
+    def __init__(self, value, callback):
+        self.xvalue = value
+        self.callback = callback
+    
+    @property 
+    def value(self):
+        return self.xvalue
+    
+    def __repr__(self):
+        return self.xvalue
+
+    @value.setter
+    def value(self, value):
+        self.xvalue = value
+        self.callback(value)
+
+def gen_reactive():
+    with open("reactive.py", "w+") as reactiveFile:
+        reactiveFile.write("".join(reactive))
 
 class Component:
     def __init__(self):
         self.extra = ""
         self.children = []
-        self.tags = {}
+        self.uuid = gen_uuid()
+        self.tags = {"id": self.uuid}
 
     def set(self, key, value):
         self.tags[key] = value.__name__+"()"
+
+    def onclick(self, bound_to, element=None):
+        def decorator_onclick(func):
+            @wraps(func)
+            def wrapper_onclick(*args, **kwargs):
+                self.bound_to = bound_to
+                if "intialize" not in list(kwargs.keys()):
+                    func(*args, **kwargs)
+
+                if element != None:
+                    element.set("onclick", func)
+                else:
+                    self.set("onclick", func)
+
+                # ASSUMES self.text **IS** a string, **NOT** a list
+                x = compile("{[a-z]+}")
+                varname = x.search(self.children)[0][1:-1]
+                reactive.append(f"""{varname} = AutoRender({bound_to}, update, "{self.children}", "{self.uuid}")\n\n""")
+                reactive.append("\n".join(getsource(func).split("\n")[1:])+"\n\n")
+            return wrapper_onclick
+        return decorator_onclick
 
     def render(self):
         for key,value in self.tags.items():
@@ -16,6 +66,14 @@ class Component:
         children_str = ""
         for child in self.children:
             children_str += str(child)
+
+        if children_str.__contains__("{") and children_str.__contains__("}"):
+            z = compile("{[a-z]+}")
+            z = z.findall(children_str)
+            for i in z:
+                children_str = children_str.replace(i, "{}")
+            children_str = children_str.format(self.bound_to)
+
         return f"<{tag}{self.extra}>{children_str}</{tag}>"
 
     def __repr__(self):
@@ -30,6 +88,7 @@ class Page(Component):
         self.children = [f"\n\t<head>\n\t\t<title>{title}</title>\n\t\t<link href='/pynetic.css' rel='stylesheet'>\n\t\t<script src=\"https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js\"></script>\n\t</head>\n\t<body>", """<script src="/pynetic.js"></script>"""]
 
     def render(self):
+        end = time()
         body = ""
         i = 0
         for child in self.body:
@@ -41,6 +100,10 @@ class Page(Component):
         children_str = ""
         for child in self.children:
             children_str += str(child)
+        
+    
+
+        print(f"Finished in {round((end-start)*1000, 3)} ms")
         return "<!DOCTYPE HTML>\n"+f"<{tag}{self.extra}>{children_str}</{tag}>"
 
     def add(self, child):
