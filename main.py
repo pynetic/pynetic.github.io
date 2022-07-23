b5eb4845739bcb9d132e2b9c83361e8021491a4d
RhinoCodes <forsbergw82@protonmail.com>
2022-07-22 21:34:18 -0500

    Update site source

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pynetic import *
 
 index = Page(title="Pynetic Test")
 header = index.add(Header(
-    text="Hello Internet!",
+    text="Hello Internet! {x}",
     size=1
 ))
 
@@ -10,10 +10,23 @@ body = index.add(Paragraph(
     text=[Link([Bold("Pynetic")], "https://github.com/pynetic"), " aims to be a batteries included python-based framework for web development."]
 ))
 
+x = 0
 button = index.add(Button(text="Testing Reactivity!"))
-button.set(
-    "onclick", reactive.button_click
-)
+@header.onclick(bound_to=x, element=button)
+def button_click():
+    globals()["x"].value += 1
 
-with open("index.html", "w+") as index_x:
-    index_x.write(index.render())
+button_click(intialize=True)
+
+y = 100
+button2 = index.add(Button(text="Testing Reactivity! {y}"))
+@button2.onclick(bound_to=y)
+def button_click2():
+    globals()["y"].value -= 1
+
+button_click2(intialize=True)
+
+gen_reactive()
+
+with open("index.html", "w+") as indexFile:
+    indexFile.write(index.render())
